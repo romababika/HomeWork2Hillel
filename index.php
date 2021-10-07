24fb6381e2bf555764f34372e39d3d00f13228b4
romababika <babika130@gmail.com>
2021-10-07 22:51:27 +0300

    lesson3Full

--- index.php
+++ index.php
@@ -4,14 +4,23 @@ error_reporting(-1);
 
 $arr = [1, 2, 3, 7, 31, 4, 1, 8, 6];
 //посчитать длину массива
-var_dump(count($arr));
+
+echo "Длина массива :".(count($arr));
 echo '<br>';
 
 //переместить первые 4 элемента массива в конец массива
+array_push($arr,'1','2','3','7');
+unset($arr[0],$arr[1],$arr[2],$arr[3]);
+echo '<pre>';
+print_r($arr);
+echo '</pre>';
+
 
 //получить сумму 4,5,6 элемента
-$sum = array_sum(array_slice($arr, 3, 3, true));
+
+$sum = $arr[3]+$arr[4]+$arr[5];
 echo "Сумма  елементов :{$sum}";
+echo '<br>';
 
 
 $firstArr = [
@@ -29,20 +38,36 @@ $secondArr = [
     'five' => 13,
     'six' => 37,
 ];
+
 //найти все элементы которые отсутствуют в первом массиве и присутствуют во втором
-$result = count(array_diff($firstArr, $secondArr));
+
+$result = array_diff(array_keys($secondArr),array_keys($firstArr));
+echo '<pre>';
 print_r($result);
+echo '</pre>';
 echo '<br>';
 
 //найти все элементы которые присутствую в первом и отсутствуют во втором
 
+$result = array_diff(array_keys($firstArr),array_keys($secondArr));
+echo '<pre>';
+print_r($result);
+echo '</pre>';
+echo '<br>';
+
 //найти все элементы значения которых совпадают
+
 $count = count(array_intersect($firstArr, $secondArr));
-print_r($count);
+echo "Элементы значения которых совпадают : {$count}";
 echo '<br>';
 
 //найти все элементы значения которых отличается
 
+$result = array_diff_assoc($firstArr, $secondArr);
+echo '<pre>';
+var_dump($result);
+echo '</pre>';
+echo '<br>';
 
 $firstArr = [
     'one' => 1,
@@ -62,8 +87,40 @@ $firstArr = [
         'five' => 12,
     ],
 ];
+
 //получить все вторые элементы вложенных массивов
+
+foreach ($firstArr as $item) {
+    if (!is_array($item)) {
+        continue;
+    }
+    $second_item = $item[array_keys($item)[1]];
+    echo '<pre>';
+    var_dump(array_keys($item)[1]);
+    var_dump($second_item);
+    echo '</pre>';
+}
+echo '<br>';
 
 //получить общее количество элементов в массиве
 
+$mass=count($firstArr, COUNT_RECURSIVE);
+echo "Общее количество елементов в массиве: {$mass}";
+echo '<br>';
+
 //получить сумму всех значений в массиве
+
+function sumArr($firstArr) {
+    $sum = 0;
+    foreach($firstArr as $v) {
+        if (is_array($v)) {
+            $sum += sumArr($v);
+        } else {
+            $sum += $v;
+        }
+    }
+    return $sum;
+}
+$s=sumArr($firstArr);
+echo "сумма всех значений в массиве :{$s}";
+echo '<br>';
