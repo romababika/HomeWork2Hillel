d7dd8e8ea19c2ab6af83e94596ecb29b080ec40f
romababika <babika130@gmail.com>
2021-10-10 22:51:33 +0300

    HomeWorkFullLesson4

--- index.php
+++ index.php
@@ -2,117 +2,82 @@
 
 error_reporting(-1);
 
-$arr = [1, 2, 3, 7, 31, 4, 1, 8, 6];
-//посчитать длину массива
-
-echo "Длина массива :".(count($arr));
-echo '<br>';
-
-//переместить первые 4 элемента массива в конец массива
-array_push($arr,'1','2','3','7');
-unset($arr[0],$arr[1],$arr[2],$arr[3]);
-echo '<pre>';
-print_r($arr);
-echo '</pre>';
-
-
-//получить сумму 4,5,6 элемента
-
-$sum = $arr[3]+$arr[4]+$arr[5];
-echo "Сумма  елементов :{$sum}";
-echo '<br>';
-
-
-$firstArr = [
+$array= [
     'one' => 1,
-    'two' => 2,
     'three' => 3,
     'foure' => 5,
     'five' => 12,
-];
-$secondArr = [
-    'one' => 1,
-    'seven' => 22,
-    'three' => 32,
-    'foure' => 5,
-    'five' => 13,
-    'six' => 37,
-];
-
-//найти все элементы которые отсутствуют в первом массиве и присутствуют во втором
-
-$result = array_diff(array_keys($secondArr),array_keys($firstArr));
-echo '<pre>';
-print_r($result);
-echo '</pre>';
-echo '<br>';
-
-//найти все элементы которые присутствую в первом и отсутствуют во втором
-
-$result = array_diff(array_keys($firstArr),array_keys($secondArr));
-echo '<pre>';
-print_r($result);
-echo '</pre>';
-echo '<br>';
-
-//найти все элементы значения которых совпадают
-
-$count = count(array_intersect($firstArr, $secondArr));
-echo "Элементы значения которых совпадают : {$count}";
-echo '<br>';
-
-//найти все элементы значения которых отличается
-
-$result = array_diff_assoc($firstArr, $secondArr);
-echo '<pre>';
-var_dump($result);
-echo '</pre>';
-echo '<br>';
-
-$firstArr = [
-    'one' => 1,
+    'six' => 1,//22
     'two' => [
-        'one' => 1,
         'seven' => 22,
-        'three' => 32,
+        'elewen' => 22,//44
     ],
-    'three' => [
-        'one' => 1,
-        'two' => 2,
+    'eleven' => [
+        'hungri' => 1,
+        'ader' => 2,//3
     ],
-    'foure' => 5,
-    'five' => [
-        'three' => 32,
-        'foure' => 5,
-        'five' => 12,
-    ],
-];
+    'lmkj' => 5,
+    'fivepoiyu' => [
+        'threemnhy' => 32,
+        'fourendgfer' => 5,
+        'fiverwesd' => 12,//54+69=123
+],];
 
-//получить все вторые элементы вложенных массивов
 
-foreach ($firstArr as $item) {
-    if (!is_array($item)) {
-        continue;
+$str="bla bla bla books debug";
+//$str=0265; Для проверки
+//$str="2569874563215897";  Для проверки
+
+
+
+//Создать функцию принимающую массив произвольной вложенности и определяющий любой элемент номер которого передан параметром во всех вложенных массивах.
+function arraySearch($array, $find)
+{
+    $r = [];
+    foreach ($array as $key => $item) {
+
+        if (is_array($item)) {
+            $r = array_merge($r, arraySearch($item, $find));
+        }
+        if ($key == $find) {
+            $r[] = $item;
+        }
     }
-    $second_item = $item[array_keys($item)[1]];
-    echo '<pre>';
-    var_dump(array_keys($item)[1]);
-    var_dump($second_item);
-    echo '</pre>';
+
+    return $r;
+
 }
+echo '<pre>';
+echo "Елемент  равен ";
+echo '<br>';
+var_dump(arraySearch($array,'threemnhy'));
+echo '</pre>';
+
+//Создать функцию которая считает все буквы b в переданной строке, в случае если передается не строка функция должна возвращать false
+
+function countsLetters(string $str,$letter)
+{
+
+    if (is_numeric($str)) {
+        echo "Это не строка из букв ";
+        echo '<br>';
+        return false;
+    }
+    if (strpos($str, $letter) !== false) {
+        echo ' Символ "'.$letter.'" есть в данной строке.'.' ' ;
+        echo "Встретился  " .mb_substr_count($str, $letter). ' '. "раз";
+    } else {
+        echo 'Символа "'.$letter.'" нет в данной строке.';
+    }
+}
+echo countsLetters($str,'b');
 echo '<br>';
 
-//получить общее количество элементов в массиве
+//Создать функцию которая считает сумму значений всех элементов массива произвольной глубины
 
-$mass=count($firstArr, COUNT_RECURSIVE);
-echo "Общее количество елементов в массиве: {$mass}";
-echo '<br>';
-
-//получить сумму всех значений в массиве
-
-function sumArr($firstArr) {
+function sumArr($array) {
     $sum = 0;
-    foreach($firstArr as $v) {
+    foreach($array as $v) {
         if (is_array($v)) {
             $sum += sumArr($v);
         } else {
@@ -121,6 +86,26 @@ function sumArr($firstArr) {
     }
     return $sum;
 }
-$s=sumArr($firstArr);
-echo "сумма всех значений в массиве :{$s}";
+$sumNumbers=sumArr($array);
+echo "Сумма значений всех элементов массива  :{$sumNumbers}";
 echo '<br>';
+
+
+//Создать функцию которая определит сколько квадратов меньшего размера можно вписать в квадрат большего размера размер возвращать в float
+
+function sguareCount($sguare1,float $sguare2 )
+{
+    $P1=$sguare1*$sguare1;
+    $P2=$sguare2*$sguare2;
+    if ($P2/$P1>=1)
+    {
+         $countSguare=$P2/$P1;
+         echo "В квадрат размером {$sguare2} влезит {$countSguare} меньших квадратов";
+    }elseif ($P2/$P1<=1)
+    {
+        echo "Не влезит ни один квадрат";
+    }
+
+
+}
+echo sguareCount(5,10);
